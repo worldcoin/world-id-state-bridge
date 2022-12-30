f1d728e7100fcb7ceabfe167d6fb5db928e0704b
dcbuilder.eth <dcbuilder@protonmail.com>
2022-12-30 01:04:40 +0100

    add(future proof)

--- src/IBridge.sol
+++ src/IBridge.sol
@@ -1,10 +1,16 @@
 pragma solidity >=0.8.4;
 
 interface IBridge {
+    /// @notice Sends the latest Semaphore root to all chains.
+    /// @dev Calls this method on the L1 Proxy contract to relay roots and timestamps to WorldID supported chains.
+    /// @param root The latest Semaphore root.
+    /// @param timestamp The Ethereum block timestamp of the latest Semaphore root.
+    function sendRootMultichain(uint256 root, uint128 timestamp) external;
+
     /// @notice Sends the latest Semaphore root to Optimism.
     /// @dev Calls this method on the L1 Proxy contract to relay roots and timestamps to Optimism
     // using the ICrossICrossDomainMessenger.sol interface.
     /// @param root The latest Semaphore root.
     /// @param timestamp The Ethereum block timestamp of the latest Semaphore root.
-    function sendRootToOptimism(uint256 root, uint128 timestamp) external {}
+    function sendRootToOptimism(uint256 root, uint128 timestamp) internal;
 }
