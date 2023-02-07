db8db3fdc209d8927255803c8ddab9233483810c
dcbuilder.eth <dcbuilder@protonmail.com>
2023-02-07 19:54:01 +0000

    add(transferOwnership)

---
+++ src/interfaces/ICrossDomainOwnable3.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.8.4;
+
+/// @notice Interface for the CrossDomainOwnable contract
+/// @dev Adds functionality to the StateBridge to transfer ownership
+/// of OpWorldID to another contract on L1 or to a local Optimism EOA
+interface ICrossDomainOwnable3 {
+    /// @notice transfers owner to a cross-domain or local owner
+    /// @param _owner new owner (EOA or contract)
+    /// @param _isLocal true if new owner is on Optimism, false if it is a cross-domain owner
+    function transferOwnership(address _owner, bool _isLocal) external;
+}
---
+++ src/interfaces/IOpWorldID.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/// @title Interface for the OpWorldID contract
+interface IOpWorldID {
+    /// @notice receiveRoot is called by the L1 Proxy contract which forwards new Semaphore roots to L2.
+    /// @param newRoot new valid root with ROOT_HISTORY_EXPIRY validity
+    /// @param timestamp Ethereum block timestamp of the new Semaphore root
+    function receiveRoot(uint256 newRoot, uint128 timestamp) external;
+}
