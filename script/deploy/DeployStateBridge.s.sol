b9023aeca907d5b818ee9ef7dfa9c0da097117e9
dcbuilder.eth <dcbuilder@protonmail.com>
2023-02-14 23:48:16 +0100

    fix(forge build)

--- script/deploy/DeployStateBridge.s.sol
+++ script/deploy/DeployStateBridge.s.sol
@@ -9,12 +9,21 @@ import {StateBridge} from "../../src/StateBridge.sol";
 contract DeployStateBridge is Script {
     StateBridge public bridge;
 
+    address public checkpointManagerAddress;
+    address public fxRootAddress;
+
+    function setup() public {
+        // TBD
+        checkpointManagerAddress = address(0x38b421a8A92375A356224F15CDE7AA94F64d371a);
+        fxRootAddress = address(0x38b421a8A92375A356224F15CDE7AA94F64d371a);
+    }
+
     function run() public {
         uint256 bridgeKey = vm.envUint("BRIDGE_PRIVATE_KEY");
 
         vm.startBroadcast(bridgeKey);
 
-        bridge = new StateBridge();
+        bridge = new StateBridge(checkpointManagerAddress, fxRootAddress);
 
         vm.stopBroadcast();
     }
