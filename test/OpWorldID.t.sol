9a6ed2cea5aa5f94ee69862c52482cd8c45cc47b
dcbuilder.eth <dcbuilder@protonmail.com>
2023-02-20 14:59:29 +0100

    add(CrossDomainOwnable3)

--- test/OpWorldID.t.sol
+++ test/OpWorldID.t.sol
@@ -4,14 +4,13 @@ pragma solidity 0.8.15;
 /// @dev using Test from forge-std which is inherited from Optimism's CommonTest.t.sol
 // import { PRBTest } from "@prb/test/PRBTest.sol";
 // import { StdCheats } from "forge-std/StdCheats.sol";
-import {OpWorldID} from "../src/OpWorldID.sol";
-import {L2CrossDomainMessenger} from "@eth-optimism/contracts-bedrock/contracts/L2/L2CrossDomainMessenger.sol";
-import {CrossDomainOwnable2} from "@eth-optimism/contracts-bedrock/contracts/L2/CrossDomainOwnable2.sol";
-import {Predeploys} from "@eth-optimism/contracts-bedrock/contracts/libraries/Predeploys.sol";
-import {CommonTest, Messenger_Initializer} from "@eth-optimism/contracts-bedrock/contracts/test/CommonTest.t.sol";
-import {AddressAliasHelper} from "@eth-optimism/contracts-bedrock/contracts/vendor/AddressAliasHelper.sol";
-import {Encoding} from "@eth-optimism/contracts-bedrock/contracts/libraries/Encoding.sol";
-import {Bytes32AddressLib} from "solmate/src/utils/Bytes32AddressLib.sol";
+import { OpWorldID } from "../src/OpWorldID.sol";
+import { L2CrossDomainMessenger } from "@eth-optimism/contracts-bedrock/contracts/L2/L2CrossDomainMessenger.sol";
+import { Predeploys } from "@eth-optimism/contracts-bedrock/contracts/libraries/Predeploys.sol";
+import { CommonTest, Messenger_Initializer } from "@eth-optimism/contracts-bedrock/contracts/test/CommonTest.t.sol";
+import { AddressAliasHelper } from "@eth-optimism/contracts-bedrock/contracts/vendor/AddressAliasHelper.sol";
+import { Encoding } from "@eth-optimism/contracts-bedrock/contracts/libraries/Encoding.sol";
+import { Bytes32AddressLib } from "solmate/src/utils/Bytes32AddressLib.sol";
 
 /// @title OpWorldIDTest
 /// @author Worldcoin
@@ -31,14 +30,21 @@ contract OpWorldIDTest is Messenger_Initializer {
     /// @notice The root of the merkle tree after the first update
     uint256 public newRoot = 0x5c1e52b41a571293b30efacd2afdb7173b20cfaf1f646c4ac9f96eb75848270;
 
+    /// @notice OpenZeppelin Ownable.sol transferOwnership event
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /// @notice CrossDomainOwnable3.sol transferOwnership event
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner, bool isLocal);
+
     function setUp() public override {
-        /// @notice CrossDomainOwnable2 setup
+        /// @notice CrossDomainOwnable3 setup
         super.setUp();
 
         /// @notice The timestamp of the root of the merkle tree before the first update
         uint128 preRootTimestamp = uint128(block.timestamp);
 
         /// @notice Initialize the OpWorldID contract
+        vm.prank(alice);
         id = new OpWorldID();
 
         id.initialize(preRoot, preRootTimestamp);
@@ -48,28 +54,50 @@ contract OpWorldIDTest is Messenger_Initializer {
         vm.label(address(id), "OPWorldID");
     }
 
+    function _switchToLocalOwnership(OpWorldID id) internal {
+        vm.expectEmit(true, true, true, true);
+
+        // OpenZeppelin Ownable.sol transferOwnership event
+        emit OwnershipTransferred(alice, alice);
+
+        // CrossDomainOwnable3.sol transferOwnership event
+        emit OwnershipTransferred(alice, alice, false);
+
+        // CrossDomainOwnable3.sol transferOwnership to crossDomain address (as alice and to alice)
+        vm.prank(id.owner());
+        id.transferOwnership({ _owner: alice, _isLocal: false });
+    }
+
     function test_onlyOwner_notMessenger_reverts() external {
+        _switchToLocalOwnership(id);
+
         uint128 newRootTimestamp = uint128(block.timestamp + 100);
 
-        vm.expectRevert("CrossDomainOwnable2: caller is not the messenger");
+        // calling locally (not as the messenger)
+        vm.prank(bob);
+        vm.expectRevert("CrossDomainOwnable3: caller is not the messenger");
         id.receiveRoot(newRoot, newRootTimestamp);
     }
 
     function test_onlyOwner_notOwner_reverts() external {
-        // set the xDomainMsgSender storage slot
+        _switchToLocalOwnership(id);
+
+        // set the xDomainMsgSender storage slot as bob
         bytes32 key = bytes32(uint256(204));
-        bytes32 value = Bytes32AddressLib.fillLast12Bytes(address(alice));
+        bytes32 value = Bytes32AddressLib.fillLast12Bytes(address(bob));
         vm.store(address(L2Messenger), key, value);
 
         uint128 newRootTimestamp = uint128(block.timestamp + 100);
 
         vm.prank(address(L2Messenger));
-        vm.expectRevert("CrossDomainOwnable2: caller is not the owner");
+        vm.expectRevert("CrossDomainOwnable3: caller is not the owner");
         id.receiveRoot(newRoot, newRootTimestamp);
     }
 
     /// @notice Test that you can insert new root and check if it is valid
     function test_receiveVerifyRoot_succeeds() public {
+        _switchToLocalOwnership(id);
+
         address owner = id.owner();
         uint128 newRootTimestamp = uint128(block.timestamp + 100);
         vm.warp(block.timestamp + 200);
@@ -90,6 +118,8 @@ contract OpWorldIDTest is Messenger_Initializer {
 
     /// @notice Test that a root that hasn't been inserted is invalid
     function test_receiveVerifyInvalidRoot_reverts() public {
+        _switchToLocalOwnership(id);
+
         address owner = id.owner();
 
         uint128 newRootTimestamp = uint128(block.timestamp + 100);
@@ -113,6 +143,8 @@ contract OpWorldIDTest is Messenger_Initializer {
 
     /// @notice Test that you can insert a root and check it has expired if more than 7 days have passed
     function test_expiredRoot_reverts() public {
+        _switchToLocalOwnership(id);
+
         address owner = id.owner();
 
         uint128 newRootTimestamp = uint128(block.timestamp + 100);
