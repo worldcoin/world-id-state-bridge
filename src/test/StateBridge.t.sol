ea208d64338d08e22a49f4390c05c1a599c75c88
dcbuilder.eth <dcbuilder@protonmail.com>
2023-03-13 08:43:52 +0000

    fix(small lint errors)

--- src/test/StateBridge.t.sol
+++ src/test/StateBridge.t.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.15;
 
-import {UUPSUpgradeable} from "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
-import {ERC1967Upgrade} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Upgrade.sol";
-import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { UUPSUpgradeable } from "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
+import { ERC1967Upgrade } from "openzeppelin-contracts/proxy/ERC1967/ERC1967Upgrade.sol";
+import { ERC1967Proxy } from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-import {StateBridge} from "src/StateBridge.sol";
-import {StateBridge2} from "./StateBridge2.sol";
-import {StateBridgeProxy} from "src/StateBridgeProxy.sol";
+import { StateBridge } from "src/StateBridge.sol";
+import { StateBridge2 } from "./StateBridge2.sol";
+import { StateBridgeProxy } from "src/StateBridgeProxy.sol";
 
-import {console2} from "forge-std/console2.sol";
-import {PRBTest} from "@prb/test/PRBTest.sol";
-import {StdCheats} from "forge-std/StdCheats.sol";
+import { PRBTest } from "@prb/test/PRBTest.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
 
 contract StateBridgeTest is PRBTest, StdCheats {
     address public testSemaphoreAddress;
@@ -31,8 +30,6 @@ contract StateBridgeTest is PRBTest, StdCheats {
     }
 
     function testBridgeUpgrade() public {
-        console2.log("testBridgeUpgrade");
-
         // deploy StateBridge
         StateBridge stateBridge = new StateBridge(checkpointManagerAddress, fxRootAddress);
 
@@ -40,12 +37,7 @@ contract StateBridgeTest is PRBTest, StdCheats {
 
         bytes memory initCallData = abi.encodeCall(
             StateBridge.initialize,
-            (
-                testSemaphoreAddress,
-                testOptimismAddress,
-                testPolygonAddress,
-                crossDomainMessengerAddress
-            )
+            (testSemaphoreAddress, testOptimismAddress, testPolygonAddress, crossDomainMessengerAddress)
         );
 
         // deploy StateBridgeProxy
@@ -57,12 +49,7 @@ contract StateBridgeTest is PRBTest, StdCheats {
 
         initCallData = abi.encodeCall(
             StateBridge2.initialize,
-            (
-                testSemaphoreAddress,
-                testOptimismAddress,
-                testPolygonAddress,
-                crossDomainMessengerAddress
-            )
+            (testSemaphoreAddress, testOptimismAddress, testPolygonAddress, crossDomainMessengerAddress)
         );
 
         (bool success, bytes memory result) = stateBridgeProxyAddress.call(
@@ -71,8 +58,7 @@ contract StateBridgeTest is PRBTest, StdCheats {
 
         assert(success);
 
-        (success, result) =
-            stateBridgeProxyAddress.call(abi.encodeCall(StateBridge2.getCounter, ()));
+        (success, result) = stateBridgeProxyAddress.call(abi.encodeCall(StateBridge2.getCounter, ()));
 
         assert(success);
 
