f8ad2968b638374178973c5cdbed283247d2ee02
mmqxyz <127844428+mmqxyz@users.noreply.github.com>
2023-03-24 22:33:09 +0100

    Fix typo in SemaphoreTreeDepthValidator.sol

--- src/utils/SemaphoreTreeDepthValidator.sol
+++ src/utils/SemaphoreTreeDepthValidator.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.15;
 /// @title Semaphore tree depth validator
 /// @author Worldcoin
 library SemaphoreTreeDepthValidator {
-    /// @notice Checks if the provided `treeDepth` is amoung supported depths.
+    /// @notice Checks if the provided `treeDepth` is among supported depths.
     ///
     /// @param treeDepth The tree depth to validate.
     /// @return supportedDepth Returns `true` if `treeDepth` is between 16 and 32 - depths supported by the Semaphore
