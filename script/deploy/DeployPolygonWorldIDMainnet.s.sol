5798f90dd2f8ed12995dee7c640ddde8106db57b
dcbuilder.eth <dcbuilder@protonmail.com>
2023-03-27 17:25:55 +0100

    fixes

---
+++ script/deploy/DeployPolygonWorldIDMainnet.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+// Demo deployments
+// Goerli 0x09A02586dAf43Ca837b45F34dC2661d642b8Da15
+// https://goerli-optimism.etherscan.io/address/0x09a02586daf43ca837b45f34dc2661d642b8da15#code
+
+import {Script} from "forge-std/Script.sol";
+import {PolygonWorldID} from "../../src/PolygonWorldID.sol";
+
+contract DeployPolygonWorldID is Script {
+    address public stateBridgeAddress;
+
+    // Polygon PoS Mainnet Child Tunnel
+    address fxChildAddress = address(0x8397259c983751DAf40400790063935a11afa28a);
+
+    PolygonWorldID public polygonWorldId;
+    uint256 public privateKey;
+
+    /*//////////////////////////////////////////////////////////////
+                                CONFIG
+    //////////////////////////////////////////////////////////////*/
+    string public root = vm.projectRoot();
+    string public path = string.concat(root, "/script/.deploy-config.json");
+    string public json = vm.readFile(path);
+
+    function setUp() public {
+        privateKey = abi.decode(vm.parseJson(json, ".privateKey"), (uint256));
+    }
+
+    function run() external {
+        vm.startBroadcast(privateKey);
+
+        polygonWorldId = new PolygonWorldID(fxChildAddress);
+
+        vm.stopBroadcast();
+    }
+}
