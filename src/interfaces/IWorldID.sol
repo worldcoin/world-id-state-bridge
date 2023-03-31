5591509d8cbd8e20ae451fdccd34d6f065900840
Ara Adkins <me@ara.io>
2023-03-31 14:34:59 -0600

    Initial WorldIDBridge abstract base class

--- src/interfaces/IWorldID.sol
+++ src/interfaces/IWorldID.sol
@@ -1,21 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+/// @title WorldID Interface
+/// @author Worldcoin
+/// @notice The interface to the proof verification for WorldID.
 interface IWorldID {
-    function sendRootToStateBridge(uint256 root) external;
-
-    /// @notice Reverts if the zero-knowledge proof is invalid.
+    /// @notice Verifies a WorldID zero knowledge proof.
+    /// @dev Note that a double-signaling check is not included here, and should be carried by the
+    ///      caller.
+    /// @dev It is highly recommended that the implementation is restricted to `view` if possible.
+    ///
     /// @param root The of the Merkle tree
     /// @param signalHash A keccak256 hash of the Semaphore signal
     /// @param nullifierHash The nullifier hash
     /// @param externalNullifierHash A keccak256 hash of the external nullifier
     /// @param proof The zero-knowledge proof
-    /// @dev  Note that a double-signaling check is not included here, and should be carried by the caller.
+    ///
+    /// @custom:reverts string If the `proof` is invalid.
     function verifyProof(
         uint256 root,
         uint256 signalHash,
         uint256 nullifierHash,
         uint256 externalNullifierHash,
         uint256[8] calldata proof
-    ) external view;
+    ) external;
 }
