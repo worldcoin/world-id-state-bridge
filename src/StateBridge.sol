af517425da0ca9e194afd90a2d04120d710aa255
dcbuilder.eth <dcbuilder@protonmail.com>
2023-05-06 11:25:20 +0200

    fix(error)

--- src/StateBridge.sol
+++ src/StateBridge.sol
@@ -59,9 +59,6 @@ contract StateBridge is FxBaseRootTunnel, Ownable {
     /// @notice Thrown when the caller of `sendRootMultichain` is not the WorldID Identity Manager contract.
     error NotWorldIDIdentityManager();
 
-    /// @notice Thrown when setWorldIDAddress is called with a zero address
-    error CantSetWorldIDAddressToZero();
-
     ///////////////////////////////////////////////////////////////////
     ///                          MODIFIERS                          ///
     ///////////////////////////////////////////////////////////////////
@@ -224,17 +221,5 @@ contract StateBridge is FxBaseRootTunnel, Ownable {
     function setFxChildTunnel(address _fxChildTunnel) public virtual override onlyOwner {
         require(fxChildTunnel == address(0x0), "FxBaseRootTunnel: CHILD_TUNNEL_ALREADY_SET");
         fxChildTunnel = _fxChildTunnel;
-    }
-
-    /// @notice Sets the WorldIDManagerRouter address
-    /// @param _worldIDIdentityManagerAddress The new WorldID Identity Manager Router address
-    function setWorldIDIdentityManagerAddress(address _worldIDIdentityManagerAddress)
-        public
-        onlyOwner
-    {
-        if (worldIDAddress == address(0x0)) {
-            revert CantSetWorldIDAddressToZero();
-        }
-        worldIDAddress = _worldIDIdentityManagerAddress;
     }
 }
