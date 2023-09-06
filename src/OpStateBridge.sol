3c232b3b281aa0d9ec11dea711fd573d9c1d1cb4
0xKitsune <0xKitsune@protonmail.com>
2023-09-06 16:07:08 -0400

    chore: fix natspec on GasLimitZero error

--- src/OpStateBridge.sol
+++ src/OpStateBridge.sol
@@ -81,7 +81,7 @@ contract OpStateBridge is Ownable2Step {
     /// @notice Emitted when an attempt is made to renounce ownership.
     error CannotRenounceOwnership();
 
-    /// @notice Emitted when an attempt is made to set the gas limit to zero or less
+    /// @notice Emitted when an attempt is made to set the gas limit to zero
     error GasLimitZero();
 
     /// @notice Emitted when an attempt is made to set an address to zero
