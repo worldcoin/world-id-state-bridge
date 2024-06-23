41d2a5ef7bcf487e4aa68cb85e027af519a1e535
James Harrison <somtochukwujames2000@gmail.com>
2024-06-23 20:41:41 +0100

    chores: added linea state bridge contract and documented them

---
+++ src/interfaces/ILineaWorldID.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/// @title Interface for the LineaWorldID contract
+/// @author Worldcoin
+/// @notice Interface for the CrossDomainOwnable contract for the Optimism L2
+/// @custom:usage abi.encodeCall(ILineaWorldID.receiveRoot, (_newRoot, _supersedeTimestamp));
+interface ILineaWorldID {
+    ////////////////////////////////////////////////////////////////////////////////
+    ///                               ROOT MIRRORING                            ///
+    ///////////////////////////////////////////////////////////////////////////////
+
+    /// @notice This function is called by the state bridge contract when it forwards a new root to
+    ///         the bridged WorldID.
+    ///
+    /// @param newRoot The value of the new root.
+    ///
+    /// @custom:reverts CannotOverwriteRoot If the root already exists in the root history.
+    /// @custom:reverts string If the caller is not the owner.
+    function receiveRoot(uint256 newRoot) external;
+}
